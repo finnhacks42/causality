7f9c20b24ae11d1b8df8a4060a788a02f7341ca0
finn42 <finnlattimore@gmail.com>
2015-10-22 11:05:28 +1100

    prep for tpr talk

--- unknown_q.py
+++ unknown_q.py
@@ -10,35 +10,60 @@ def calc_m(array): # assumes array already sorted smallest to largest
     print("returning len")
     return len(array)
 
-N = 100 # Number of variables
-m = 10 # Number of unbalanced variables
-q = zeros(N)
-q[m-1:] = 1/m
+def simulate_estimation_of_q_and_m(N,m,delta,simulations):
+    # simulate observering for h rounds and estimating bar(q_i)
+    q = zeros(N)
+    q[m-1:] = 1/m
+    h = 12*m*log(4*N/delta)
+    print("h",h)
+    est_m = zeros(simulations)
+    for i in range(simulations):
+        counts = binom.rvs(h,q) #For each q_i the the count will be ~ binomial(h,q_i)
+        est_q = counts/h
+        est_bar_q = sort(minimum(est_q,1-est_q)) #the algorithm doesn't know q_i < .5
+        est_m[i] = calc_m(est_bar_q)
 
-# simulate observering for h rounds and estimating bar(q_i)
-delta = .05
-h = floor(N*m*log(1/delta))
-print(h)
-simulations = 1000
+    freq = stats.itemfreq(est_m)
+    print(freq)
+    fig,ax = plt.subplots(1,1)
+    plt.bar(freq[:,0],freq[:,1],align='center')
+    plt.show()
 
-est_m = zeros(simulations)
-for i in range(simulations):
-    counts = binom.rvs(h,q) #For each q_i the the count will be ~ binomial(h,q_i)
-    est_q = counts/h
-    est_bar_q = sort(minimum(est_q,1-est_q)) #the algorithm doesn't know q_i < .5
-    est_m[i] = calc_m(est_bar_q)
+def m_versus_q(Nvals,simulations):
+    results = zeros((simulations, len(Nvals)))
+    for i,s in enumerate(range(simulations)):
+        for j,N in enumerate(Nvals):
+            q = random.uniform(high=0.5,size = N)
+            q = sort(q)
+            m = calc_m(q)
+            results[i,j] = m
+    f, ax = plt.subplots(1, 1)
+    error = results.std(axis=0)
+    ax.errorbar(Nvals, results.mean(axis=0), yerr=error, fmt='o')
+    #ax.plot(Nvals,results.mean(axis=0))
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    plt.show()
+    return results
 
-freq = stats.itemfreq(est_m)
-print(freq)
-fig,ax = plt.subplots(1,1)
-plt.bar(freq[:,0],freq[:,1],align='center')
-plt.show()
+#results = m_versus_q([10,100,1000,10000,100000,1000000],10)
 
-plt.subplots(1,1)
-p = linspace(0,1,50)
-v = p*(1-p)
-plt.plot(p,v)
-plt.show()
+q = random.uniform(high=0.5,size = 10)
+q = sort(q)
+set_printoptions(precision=2)
+print(q)
+print(calc_m(q))
+
+
+#N = 10000 # Number of variables
+#m = 30 # Number of unbalanced variables
+#simulate_estimation_of_q_and_m(N,m,.05,10000)
+
+# how does m scale with N (assuming q_i drawn randomly from 0,.5
+    
+
+
+
 
 
 
